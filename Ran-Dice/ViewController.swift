10c4e166adb7f430f581825914d15020227ea1c2
Lacy Simpson <lacy.d.simpson@gmail.com>
2022-11-19 11:25:46 -0600

    Initial Commit

---
+++ Ran-Dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ran-Dice
+//
+//  Created by Lacy Simpson on 11/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
